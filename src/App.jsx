1befcd6fdce2f392cc7a05f9c776dfecc6e15783
Eran Geva <eran.geva@gmail.com>
2019-11-23 18:52:08 +0100

    device type selector

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,36 @@ import React from 'react';
 
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import Paper from '@material-ui/core/Paper';
-import { List, ListItem, ListItemText } from '@material-ui/core';
+import { List, ListItem, ListItemText, InputLabel, Select, MenuItem } from '@material-ui/core';
 
 const position = [32.081128, 34.779729];
 const theDevices = [
-    { name: "Device 1", position: [32.08, 34.77] },
-    { name: "Device 2" },
-    { name: "Device 3", position: [32.07, 34.78] },
-    { name: "Device 4" },
+    {
+        type: "wind", items: [
+            { name: "Device 1", position: [32.08, 34.77] },
+            { name: "Device 2" },
+            { name: "Device 3", position: [32.07, 34.78] },
+            { name: "Device 4" }]
+    },
+    {
+        type: "water", items: [
+            { name: "Device 5", position: [31.9, 34.77] },
+            { name: "Device 6" },
+            { name: "Device 7", position: [31.99, 34.78] },
+            { name: "Device 8" }]
+    }
 ];
 
 const App = () => {
     const [selectedIndex, setSelectedIndex] = React.useState(0);
-    const [devices, setDevices] = React.useState(theDevices);
+    const [selectedType, setSelectedType] = React.useState(theDevices[0].type);
+    const [devices, setDevices] = React.useState(theDevices[0].items);
 
     return (
         <div className="App">
             <LeafletMap center={position} zoom={14}
                 style={{ width: '100%', height: '100vh' }}
-                onClick={(e) => {
+                onClick={e => {
                     let tempDevices = devices.slice();
                     tempDevices[selectedIndex].position = [e.latlng.lat, e.latlng.lng];
                     setDevices(tempDevices);
@@ -45,7 +56,26 @@ const App = () => {
                     ))
                 }
             </LeafletMap>
-            <Paper style={{ position: 'absolute', top: 50, width: 300, right: 50, bottom: 50, justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
+            <Paper
+                style={{ position: 'absolute', top: 50, width: 300, right: 50, bottom: 50, justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}
+            >
+                <div
+                    style={{ margin: 10 }}
+                >
+                    <InputLabel id="select-type">Device Type</InputLabel>
+                    <Select
+                        labelId="select-type"
+                        id="select-type"
+                        value={selectedType}
+                        onChange={e => {
+                            setSelectedType(e.target.value);
+                        }}
+                    >
+                        {
+                            theDevices.map(dev => <MenuItem key={dev.type} value={dev.type}>{dev.type}</MenuItem>)
+                        }
+                    </Select>
+                </div>
                 <List>
                     {
                         devices.map((dev, index) => <ListItem
