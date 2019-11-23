2b7acf967a8c52b631e6d9090003860ff4b688e4
Eran Geva <eran.geva@gmail.com>
2019-11-23 17:07:42 +0100

    list of selectable devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 // import './App.css';
 
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
 import Paper from '@material-ui/core/Paper';
+import { List, ListItem, ListItemText } from '@material-ui/core';
 
-const position = [32.081128, 34.779729]
+const position = [32.081128, 34.779729];
+const devices = [
+    {name: "Device 1"},
+    {name: "Device 2"},
+    {name: "Device 3"},
+    {name: "Device 4"},
+];
 
-const App = () => (
-    <div className="App">
-        <LeafletMap center={position} zoom={14} style={{ width: '100%', height: '100vh' }}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={position}>
-                <Popup>
-                    <span>
-                        A pretty CSS3 popup. <br /> Easily customizable.
+const App = () => {
+    const [selectedIndex, setSelectedIndex] = React.useState(1);
+
+    return (
+        <div className="App">
+            <LeafletMap center={position} zoom={14} style={{ width: '100%', height: '100vh' }}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        <span>
+                            A pretty CSS3 popup. <br /> Easily customizable.
                         </span>
-                </Popup>
-            </Marker>
-        </LeafletMap>
-        <Paper style={{ position: 'absolute', top: 50, width: 300, right: 50, bottom: 50, justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
-
-        </Paper>
-        {/* <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-                Edit <code>src/App.js</code> and sfsdfsdfsave to reload.
-        </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-        </a>
-        </header> */}
-    </div>
-)
+                    </Popup>
+                </Marker>
+            </LeafletMap>
+            <Paper style={{ position: 'absolute', top: 50, width: 300, right: 50, bottom: 50, justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
+                <List>
+                    {
+                        devices.map((dev, index) => <ListItem
+                            key={dev.name}
+                            button
+                            selected={selectedIndex === index}
+                            onClick={event => setSelectedIndex(index)}
+                        >
+                            <ListItemText primary={dev.name} />
+                        </ListItem>
+                        )
+                    }
+                </List>
+            </Paper>
+        </div>
+    )
+}
 
 export default App;
