671892244d4d504753002c2a7646ae23166251ed
Eran Geva <eran.geva@gmail.com>
2019-11-23 12:31:10 +0100

    paper panel on the right

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 // import './App.css';
 
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+import Paper from '@material-ui/core/Paper';
 
 const position = [32.081128, 34.779729]
 
 const App = () => (
     <div className="App">
-        <LeafletMap center={position} zoom={14} style={{width: '100%',height: '100vh'}}>
+        <LeafletMap center={position} zoom={14} style={{ width: '100%', height: '100vh' }}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -21,6 +22,9 @@ const App = () => (
                 </Popup>
             </Marker>
         </LeafletMap>
+        <Paper style={{ position: 'absolute', top: 50, width: 300, right: 50, bottom: 50, justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}>
+
+        </Paper>
         {/* <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <p>
