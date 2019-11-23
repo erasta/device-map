f0c5876c558e6c75a56af0bc61ff23c6f3bf2659
Eran Geva <eran.geva@gmail.com>
2019-11-23 17:59:22 +0100

    changing positions

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 // import './App.css';
 
 import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
@@ -6,7 +6,7 @@ import Paper from '@material-ui/core/Paper';
 import { List, ListItem, ListItemText } from '@material-ui/core';
 
 const position = [32.081128, 34.779729];
-const devices = [
+const theDevices = [
     { name: "Device 1", position: [32.08, 34.77] },
     { name: "Device 2" },
     { name: "Device 3", position: [32.07, 34.78] },
@@ -14,11 +14,19 @@ const devices = [
 ];
 
 const App = () => {
-    const [selectedIndex, setSelectedIndex] = React.useState(1);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [devices, setDevices] = React.useState(theDevices);
 
     return (
         <div className="App">
-            <LeafletMap center={position} zoom={14} style={{ width: '100%', height: '100vh' }}>
+            <LeafletMap center={position} zoom={14}
+                style={{ width: '100%', height: '100vh' }}
+                onClick={(e) => {
+                    let tempDevices = JSON.parse(JSON.stringify(devices));
+                    tempDevices[selectedIndex].position = [e.latlng.lat, e.latlng.lng];
+                    setDevices(tempDevices);
+                }}
+            >
                 <TileLayer
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
