3586b3b6887715048750a660533d63c55ca62414
Eran Geva <eran.geva@gmail.com>
2019-11-25 21:40:42 +0100

    DeviceMarker

--- src/App.jsx
+++ src/App.jsx
@@ -54,17 +54,15 @@ const DevicesOfType = ({ devices, onSelectedChange, onDisableLocation }) => {
     )
 }
 
-const DeviceMarkers = ({ devices }) =>
-    devices.map((dev) =>
-        (!dev.position) ?
-            null :
-            <Marker position={dev.position} key={dev.name}>
-                <Popup>
-                    <span>
-                        {dev.name}
-                    </span>
-                </Popup>
-            </Marker>
+const DeviceMarker = ({ device, isSelected }) =>
+    (
+        <Marker position={device.position} key={device.name}>
+            <Popup>
+                <span>
+                    {device.name + '\n' + device.position}
+                </span>
+            </Popup>
+        </Marker>
     )
 
 const App = () => {
@@ -86,7 +84,13 @@ const App = () => {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <DeviceMarkers devices={devices.find(d => d.type === selectedType).items} />
+                {
+                    devices.find(d => d.type === selectedType).items.map((dev, index) =>
+                        (!dev.position ? null :
+                            <DeviceMarker key={dev.name} device={dev} isSelected={index === selectedIndex}/>
+                        )
+                    )
+                }
             </LeafletMap>
             <Paper
                 style={{ position: 'absolute', top: 50, width: '30%', right: 50, bottom: 50, justifyContent: 'center', alignItems: 'center', zIndex: 1000 }}
