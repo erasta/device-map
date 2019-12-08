f32b1b6119203d2259c59e7ffd055cc8c9b76137
Eran Geva <eran.geva@gmail.com>
2019-12-08 21:17:33 +0100

    Updates

--- precache-manifest.9e2b5ddeafb69ae515920297bc49318c.js
+++ precache-manifest.9e2b5ddeafb69ae515920297bc49318c.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2915085cca2eb570b0bda7a381954136",
+    "revision": "55590328970a12bfc543ee9c8d517472",
     "url": "/device-map/index.html"
   },
   {
-    "revision": "539baf26036835d83390",
+    "revision": "b627d204825813a741b9",
     "url": "/device-map/static/css/main.9bc23c85.chunk.css"
   },
   {
-    "revision": "214657a3db9b714571f8",
-    "url": "/device-map/static/js/2.1868fec2.chunk.js"
+    "revision": "a48e67ec9d785fd8c023",
+    "url": "/device-map/static/js/2.6dfcc961.chunk.js"
   },
   {
-    "revision": "539baf26036835d83390",
-    "url": "/device-map/static/js/main.e8400f05.chunk.js"
+    "revision": "b627d204825813a741b9",
+    "url": "/device-map/static/js/main.6749ffdf.chunk.js"
   },
   {
     "revision": "293491e928def9c9e34a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/device-map/precache-manifest.d1b6202a18d5445f2499af0829e85c84.js"
+  "/device-map/precache-manifest.9e2b5ddeafb69ae515920297bc49318c.js"
 );
 
 self.addEventListener('message', (event) => {
